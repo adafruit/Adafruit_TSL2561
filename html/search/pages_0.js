927ea685f06be0140f35ffab627035866d345fef
Travis CI <travis@travis-ci.org>
2018-01-19 18:28:18 +0000

    Deploy code docs to GitHub Pages Travis build: 10
    Commit: c913785d70a63774f7881bb1cbd072cb191eb94f

--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['adafruit_20tsl2561_20light_2flux_20sensor_20driver',['Adafruit TSL2561 Light/Lux sensor driver',['../index.html',1,'']]],
-  ['adafruit_20tsl2561_20light_20sensor_20driver_20_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5ftsl2561_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5ftsl2561_29',['Adafruit TSL2561 Light Sensor Driver  [![Build Status](https://travis-ci.org/adafruit/Adafruit_TSL2561.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_TSL2561)',['../md__home_travis_build_adafruit__adafruit__t_s_l2561__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20tsl2561_20light_20sensor_20driver_20_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5ftsl2561_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5ftsl2561_29',['Adafruit TSL2561 Light Sensor Driver  [![Build Status](https://travis-ci.org/adafruit/Adafruit_TSL2561.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_TSL2561)',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
