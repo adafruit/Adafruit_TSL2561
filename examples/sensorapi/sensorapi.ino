8044a72c91e139a4b7bd2e2f475d7bf90a3e8ca3
Muhammad Arifur Rahman <theintelligentboy@ymail.com>
2018-10-11 23:58:16 +0600

    Added notes for VCC connection
    Added recommendation to check the datasheet for correct logic level of the sensor.

--- examples/sensorapi/sensorapi.ino
+++ examples/sensorapi/sensorapi.ino
@@ -18,7 +18,7 @@
    ===========
    Connect SCL to I2C SCL Clock
    Connect SDA to I2C SDA Data
-   Connect VCC to 3.3V or 5V (whatever your logic level is)
+   Connect VCC to 3.3V or 5V (depending on sensor's logic level, check the datasheet)
    Connect GROUND to common ground
 
    I2C Address
